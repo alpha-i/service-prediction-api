1ed609b421ceee1703d13ea61c1cb25d4f9d047e
Gabriele Alese <gabriele.alese@alpha-i.co>
2018-02-08 16:54:05 +0000

    Basic CI setup (#2)

---
+++ test-circle.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+PGPASSWORD=postgres psql -h localhost -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'test'" | grep -q 1 || psql -U postgres -h localhost -c "CREATE DATABASE test"
+celery -A test.test_app.celery worker -E --loglevel=info &
+sleep 3  # give celery time to start
+pytest test/
