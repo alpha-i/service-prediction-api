1970e6ed2362be5f283d485204c48c0c5c5b958b
Daniele Murroni <daniele.murroni@gmail.com>
2018-02-08 15:14:57 +0100

    Admin Dashboard draft

--- app/views/customer.py
+++ app/views/customer.py
@@ -1,18 +1,24 @@
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, g
 
 from app.core.auth import requires_access_token
 from app.models.files import FileUpload
 from app.models.prediction import PredictionTask
+from app.views import USER_PROFILE, USER_ID
 
 customer_blueprint = Blueprint('customer', __name__)
 
 
-# TODO: temporary view to show a html form
-@customer_blueprint.route('/<string:customer_id>/upload')
+@customer_blueprint.route('/dashboard')
 @requires_access_token
-def upload_view(customer_id):
-    return render_template('upload.html', customer_id=customer_id)
+def dashboard():
 
+    context = {
+        'user_id': g.user.id,
+        'profile': {'user_name': g.user.username, 'email': 'changeme@soon.com'},
+        'file_uploaded': FileUpload.get_for_customer(USER_ID)
+    }
+
+    return render_template('dashboard.html', **context)
 
 # TODO: temporary view to show the uploads for this customer
 @customer_blueprint.route('/<string:customer_id>/uploads')
--- app/views/predict.py
+++ app/views/predict.py
@@ -8,9 +8,9 @@ from app.tasks.predict import predict_task, prediction_failure
 predict_blueprint = Blueprint('predict', __name__)
 
 
-@predict_blueprint.route('/<string:customer_id>/<string:upload_id>', methods=['POST'])
+@predict_blueprint.route('/<string:customer_id>/<string:upload_code>', methods=['POST'])
 @requires_access_token
-def predict(customer_id, upload_id):
+def predict(customer_id, upload_code):
     """
     Submit a prediction task for a customer
     """
@@ -19,42 +19,42 @@ def predict(customer_id, upload_id):
     if errors:
         return jsonify(errors=errors), 400
 
-    prediction_task = predict_task.apply_async(
-        (customer_id, upload_id, prediction_request),
+    celery_prediction_task = predict_task.apply_async(
+        (customer_id, upload_code, prediction_request),
         link_error=prediction_failure.s()
     )
     return jsonify({
-        'task_id': prediction_task.id,
-        'task_status': url_for('.get_task_status', task_id=prediction_task.id, _external=True),
-        'result': url_for('.get_result', task_id=prediction_task.id, _external=True)
+        'task_code': celery_prediction_task.id,
+        'task_status': url_for('.get_task_status', task_code=celery_prediction_task.id, _external=True),
+        'result': url_for('.get_result', task_code=celery_prediction_task.id, _external=True)
     }), 202
 
 
-@predict_blueprint.route('/status/<string:task_id>')
+@predict_blueprint.route('/status/<string:task_code>')
 @requires_access_token
-def get_task_status(task_id):
+def get_task_status(task_code):
     """
     Get the status of a particular task
     """
-    prediction_task = PredictionTask.get_by_task_id(task_id)
+    prediction_task = PredictionTask.get_by_task_code(task_code)
     if prediction_task:
         return jsonify(prediction_task)
     else:
         return make_response("Task not found!"), 404
 
 
-@predict_blueprint.route('/result/<string:task_id>')
+@predict_blueprint.route('/result/<string:task_code>')
 @requires_access_token
-def get_result(task_id):
+def get_result(task_code):
     """
     Get the result of an individual task
     """
-    prediction_result = PredictionResult.get_for_task(task_id)
+    prediction_result = PredictionResult.get_for_task(task_code)
     if not prediction_result:
         return make_response("Result not found!"), 404
 
     return jsonify({
         'customer_id': prediction_result.customer_id,
-        'task_id': prediction_result.task_id,
+        'task_code': prediction_result.task_code,
         'result': prediction_result.result,
     })
