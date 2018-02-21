4a79184dc93304dbe9481f3e02a60841eb839489
Gabriele Alese <gabriele.alese@alpha-i.co>
2018-02-21 11:31:40 +0000

    New test configuration

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 docker-compose up -d
-export APP_CONFIG=local.env
+export APP_CONFIG=test.env
 PGPASSWORD=postgres psql -h localhost -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'test'" | grep -q 1 || psql -U postgres -h localhost -c "CREATE DATABASE test"
 PYTHONOPTIMIZE=1 celery -A test.test_app.celery worker -E --loglevel=info --concurrency=1 &
 sleep 3  # give celery time to start
