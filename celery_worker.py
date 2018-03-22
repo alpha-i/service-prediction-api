cd8db6da822a8e1f4ec1d73bdea2f890129ea18c
Gabriele Alese <gabriele.alese@alpha-i.co>
2018-03-22 17:39:04 +0000

    Fix leftover task in celery worker

--- celery_worker.py
+++ celery_worker.py
@@ -3,4 +3,3 @@ from app import create_app, make_celery
 app = create_app('config', register_blueprints=False)
 celery = make_celery(app)
 from app.tasks.predict import training_and_prediction_task
-from app.tasks.train import training_task
