85698496ba494aa97bbb6137189eadae17bf2979
Daniele Murroni <daniele.murroni@gmail.com>
2018-03-26 16:23:03 +0100

    Prediction status live update (#84)
    * first step of autoreloading
    
    * Live Update for Runnig Prediction

--- app/static/js/alphai.js
+++ app/static/js/alphai.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+    moment.tz.setDefault('UTC');
+})
+
 var DatasourceRefresh = {
     refreshTaskCode: function () {
 
@@ -18,22 +22,22 @@ var DatasourceRefresh = {
         });
     },
     refreshRow: function (task) {
-        var last_status_index, last_status, view_task, $row;
+        var last_status_index, last_status, $row;
 
         last_status_index = task.statuses.length - 1;
         last_status = task.statuses[last_status_index].state;
 
         var status_icon = '';
 
-        switch(last_status) {
+        switch (last_status) {
             case 'SUCCESSFUL' : {
                 status_icon = 'fa-check-circle text-green'
             }
-            break;
+                break;
             case 'FAILED': {
                 status_icon = 'fa-warning text-yellow'
             }
-            break;
+                break;
             default: {
                 status_icon = 'fa-refresh fa-spin'
             }
@@ -46,7 +50,9 @@ var DatasourceRefresh = {
         $row.attr("data-completed", task.is_completed ? "True" : "False");
         $row.append('<td scope="row">' + task.name + '</td>');
         $row.append('<td scope="row">' + task.task_code + '</td>')
-        $row.append('<td>' + task.created_at + '</td>');
+
+        var created_at = moment(task.created_at)
+        $row.append('<td>' + created_at.format("YYYY-MM-DD HH:mm:ss") + '</td>');
         $row.append('<td>' + status + '</td>');
         $row.append('<td><a href="/customer/prediction/' + task.task_code + '"><i class="fa fa-list"></i></a></td>');
 
@@ -54,4 +60,65 @@ var DatasourceRefresh = {
         $($oldRow).remove();
         $('#datasource-task-list > tbody').append($row);
     }
-}
+};
+
+var PredictionStatusRefresh = {
+
+    refreshStatuses: function () {
+
+        var $prediction_table = $('table.prediction-log');
+        var $is_completed = ($('table.prediction-log').data("completed").toLowerCase() == 'true');
+        var $task_code = $prediction_table.attr("id")
+
+        url = '/prediction/' + $task_code;
+        if (!$is_completed) {
+            $.ajax({
+                url: url,
+                success: PredictionStatusRefresh.OnResultLoad
+            })
+        }
+
+    },
+    OnResultLoad: function (prediction) {
+
+        var task_code = prediction.task_code;
+
+        var $prediction_table = $('table#' + task_code);
+        $prediction_table.attr("data-completed", prediction.is_completed)
+        $tr = $prediction_table.find('tbody tr');
+        $tr.remove();
+        $(prediction.statuses).each(function(i, status) {
+            $row = PredictionStatusRefresh.buildRow(status)
+            $prediction_table.find('tbody').append($row)
+        });
+
+        PredictionStatusRefresh.manageElapsedTime(prediction)
+    },
+    buildRow: function(status){
+        $row = $('<tr>');
+
+        var created_at = moment(status.created_at);
+        var cell = $('<td>').html(created_at.format("YYYY-MM-DD HH:mm:ss"));
+        $row.append(cell);
+
+        var cell = $('<td>').html(status.state);
+        $row.append(cell);
+
+        var message = status.message || "None";
+        var cell = $('<td>').html(message);
+        $row.append(cell);
+
+        return $row
+    },
+    manageElapsedTime: function(prediction) {
+
+        if (prediction.is_completed) {
+            $('#status-spinner').hide();
+             location.reload();
+        } else {
+            $duration =  moment.duration(moment() - moment(prediction.created_at))
+            $duration_string = $duration.format('hh') + ":" + $duration.format('mm') + ":" + $duration.format('ss')
+            $elapsed_time = $("#elapsed-prediction-time").html($duration_string);
+        }
+    }
+};
