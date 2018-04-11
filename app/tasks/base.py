057b74367affc35c8c43267ae99ecda82126b9e0
Gabriele Alese <gabriele.alese@alpha-i.co>
2018-04-11 09:48:22 +0100

    Make sure to close the db session on task failure

--- app/tasks/base.py
+++ app/tasks/base.py
@@ -4,5 +4,9 @@ from app.database import db_session
 
 
 class BaseDBTask(Task):
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        db_session.remove()
+
     def after_return(self, status, retval, task_id, args, kwargs, einfo):
         db_session.remove()
